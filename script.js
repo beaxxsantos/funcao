34cf8f9ff1ad1951de2dabff4b96162600ec7a79
beaxxsantos <beasantos.rj@outlook.com>
2022-06-24 09:46:48 -0300

    função

---
+++ script.js
@@ -0,0 +1,27 @@
+function oi(){
+    console.log('Olá')
+    }
+    
+    oi()
+    
+    
+    function nome (meu){
+        console.log(`Meu nome é ${meu}`)
+    }
+    nome('Beatriz')
+    
+    
+    function info(nome, idade, musica){
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos e meu estilo musical é ${musica}`)
+    }
+    
+    info('Beatriz', '23', 'tudo um pouco')
+    
+    
+    function favorito(filme, musica){
+        console.log(`${filme} é meu filme favorito, ${musica} é a música mais escutada`)
+    }
+    
+    favorito('Rei Leão','Da Ya Think Im Sexy')
+    
+    
